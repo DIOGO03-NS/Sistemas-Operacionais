2f085ae3e9b2271dc1289e6306fe855c54234189
Diogo Nogueira <diogonogsousa@gmail.com>
2024-11-07 15:20:10 -0300

    :bug: corrige estruturação e adiciona metodos de processo

--- atividade 1/Processo.java
+++ atividade 1/Processo.java
@@ -1,29 +1,81 @@
-import java.io.*;
-import java.util.Scanner;
+import java.util.Random;
 
 public class Processo {
-    public int PID;
-    public int TP;
-    public int CP;
-    public String EP;
-    public int NES; 
-    public int NCPU;
+    private int pid;        
+    private int tp;     
+    private int cp;          
+    private String ep;       
+    private int nes;         
+    private int nCpu;         
+    private int tempoExecucao; 
 
-    //criar um construtor que pega de parametro pid e tempo de execução 
-    public Processo(int PID, int TP) {
-        this.PID = PID;
-        this.TP = TP;
-        this.CP = 0;
-        this.EP = 0;
-        this.NES = 0;
-        this.N_CPU = 0;  
+    private static final int QUANTUM = 1000;
+    private Random random = new Random();
+
+    public Processo(int pid, int tempoExecucao) {
+        this.pid = pid;
+        this.tp = 0;
+        this.cp = 1;
+        this.ep = "Pronto";
+        this.nes = 0;
+        this.nCpu = 0;
+        this.tempoExecucao = tempoExecucao;
     }
 
-    public trocaContexto() {
-
+    public void atualizarContador() {
+        tp++;
+        cp = tp + 1;
     }
 
-        // Método para salvar os dados do processo em um arquivo TXT
+    public void executarQuantum() {
+        int ciclosExecutados = 0;
+
+        while (ciclosExecutados < QUANTUM && tp < tempoExecucao) {
+            atualizarContador();
+            ciclosExecutados++;
+
+            // Checa chance de 1% para operação de E/S
+            if (random.nextInt(100) < 1) { 
+                nes++;
+                trocaDeContexto("Bloqueado");
+                return;
+            }
+        }
+
+        nCpu++;
+
+        if (tp >= tempoExecucao) {
+            finalizarProcesso();
+        } else {
+            trocaDeContexto("Pronto");
+        }
+    }
+
+    public void finalizarProcesso() {
+        ep = "Finalizado";
+        System.out.println("Processo " + pid + " terminou sua execução.");
+        System.out.println("PID: " + pid);
+        System.out.println("TP: " + tp);
+        System.out.println("CP: " + cp);
+        System.out.println("EP: " + ep);
+        System.out.println("NES: " + nes);
+        System.out.println("N_CPU: " + nCpu);
+    }
+
+    public void trocaDeContexto(String novoEstado) {
+        salvarDados();
+        String estadoAnterior = this.ep;
+        this.ep = novoEstado;
+
+        System.out.println("Processo " + pid + ": " + estadoAnterior + " >>> " + novoEstado);
+        System.out.println("PID: " + pid);
+        System.out.println("TP: " + tp);
+        System.out.println("CP: " + cp);
+        System.out.println("EP: " + ep);
+        System.out.println("NES: " + nes);
+        System.out.println("N_CPU: " + nCpu);
+    }
+
     public void salvarDados() {
         String nomeArquivo = "processo_" + pid + ".txt";
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo))) {
@@ -39,7 +91,6 @@ public class Processo {
         }
     }
 
-    // Método para carregar os dados do processo a partir do arquivo TXT
     public void carregarDados() {
         String nomeArquivo = "processo_" + pid + ".txt";
         try (Scanner scanner = new Scanner(new File(nomeArquivo))) {
@@ -72,21 +123,4 @@ public class Processo {
             System.out.println("Erro ao carregar os dados do processo " + pid + ": Arquivo não encontrado.");
         }
     }
-
-    public void trocaDeContexto(String novoEstado) {
-    salvarDados();
-
-    // Atualiza o estado do processo para o novo estado
-    String estadoAnterior = this.ep;
-    this.ep = novoEstado;
-
-    // Exibe a troca de contexto na tela
-    System.out.println("Processo " + pid + ": " + estadoAnterior + " >>> " + novoEstado);
-    System.out.println("PID: " + pid);
-    System.out.println("TP: " + tp);
-    System.out.println("CP: " + cp);
-    System.out.println("EP: " + ep);
-    System.out.println("NES: " + nes);
-    System.out.println("N_CPU: " + nCpu);
-}
 }
