86a09b6e04fb7c34dfe2af6b3dd154f00b481efb
Diogo Nogueira <diogonogsousa@gmail.com>
2025-01-16 15:14:10 -0300

    :sparkles: adiciona FCFS, SJF e PS

---
+++ atividade grupo/FirstComeFirstServed.java
@@ -0,0 +1,55 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class FirstComeFirstServed {
+
+    public void executarFCFS(List<Processo> processos) {
+        // Ordenar processos por tempo de chegada
+        processos.sort(Comparator.comparingInt(Processo::getTempoChegada));
+
+        int tempoAtual = 0;
+        double somaEspera = 0;
+        double somaRetorno = 0;
+
+        System.out.println("FCFS - First Come First Served");
+        System.out.print("Ordem de Execução: ");
+
+        for (int i = 0; i < processos.size(); i++) {
+            Processo processo = processos.get(i);
+
+            // Adicionar ID na ordem de execução
+            System.out.print(processo.getId());
+            if (i < processos.size() - 1) {
+                System.out.print(" -> ");
+            }
+
+            // Atualizar tempos
+            if (tempoAtual < processo.getTempoChegada()) {
+                tempoAtual = processo.getTempoChegada();
+            }
+            processo.setTempoEspera(tempoAtual - processo.getTempoChegada());
+            tempoAtual += processo.getTempoExecucao();
+            processo.setTempoRetorno(tempoAtual - processo.getTempoChegada());
+
+            // Somar tempos para cálculo médio
+            somaEspera += processo.getTempoEspera();
+            somaRetorno += processo.getTempoRetorno();
+        }
+
+        System.out.println("\n");
+        System.out.println("Processo | Tempo de Espera | Tempo de Retorno");
+
+        for (Processo processo : processos) {
+            System.out.printf("%s        | %d              | %d\n",
+                    processo.getId(), processo.getTempoEspera(), processo.getTempoRetorno());
+        }
+
+        // Cálculo de médias
+        double tempoMedioEspera = somaEspera / processos.size();
+        double tempoMedioRetorno = somaRetorno / processos.size();
+
+        System.out.println();
+        System.out.printf("Tempo Médio de Espera: %.2f\n", tempoMedioEspera);
+        System.out.printf("Tempo Médio de Retorno: %.2f\n", tempoMedioRetorno);
+    }
+}
---
+++ atividade grupo/Teste.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teste {
+    public static void main(String[] args) {
+        // Criar lista de processos
+        List<Processo> processos = new ArrayList<>();
+        processos.add(new Processo("P1", 0, 5, 2));
+        processos.add(new Processo("P2", 2, 3, 1));
+        processos.add(new Processo("P3", 4, 8, 3));
+        processos.add(new Processo("P4", 5, 6, 2));
+        processos.add(new Processo("P5", 11, 8, 1));
+
+        System.out.println("------------------------------------------------");
+
+        // Instanciar o escalonador e executar o FCFS
+        FirstComeFirstServed escalonador = new FirstComeFirstServed();
+        escalonador.executarFCFS(processos);
+
+        System.out.println("------------------------------------------------");
+
+        // Instanciar o escalonador e executar o SJF
+        ShortestJobFirst escalonador2 = new ShortestJobFirst();
+        escalonador2.executarSJF(processos);
+
+        System.out.println("------------------------------------------------");
+
+        // Instanciar o escalonador e executar o Priority Scheduling
+        PriorityScheduling escalonador3 = new PriorityScheduling();
+        escalonador3.executarPriorityScheduling(processos);
+
+        System.out.println("------------------------------------------------");
+        
+    }
+}
